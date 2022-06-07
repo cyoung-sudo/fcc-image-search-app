5930db1e10e1d14deea8141b7c0abe0275cd9703
Christopher Young <christopheryoung@MacBook123.local>
2022-06-06 19:24:17 -0700

    changes for heroku

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/images"));
+
+if(process.env.NODE_ENV === 'production') {
+  // Serve static files from the client folder
+  app.use(express.static('client/build'));
+  // Load the React "index" for any "GET" routes not defined above
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
  
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
